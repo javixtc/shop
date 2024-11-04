66b839ca0f59b80e32da009e89343bcb74e14b4f
Javier Vidal <javiervidal1985@gmail.com>
2024-11-04 14:18:14 +0100

    [ORDERS] WebClient to connect microservice INVENTORY

--- orders-service/src/main/java/com/javixtc/orders_service/services/OrderService.java
+++ orders-service/src/main/java/com/javixtc/orders_service/services/OrderService.java
@@ -3,7 +3,9 @@ package com.javixtc.orders_service.services;
 import java.util.UUID;
 
 import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
 
+import com.javixtc.orders_service.model.dtos.BaseResponse;
 import com.javixtc.orders_service.model.dtos.OrderItemRequest;
 import com.javixtc.orders_service.model.dtos.OrderRequest;
 import com.javixtc.orders_service.model.entities.Order;
@@ -17,17 +19,28 @@ import lombok.RequiredArgsConstructor;
 public class OrderService {
 
     private final OrderRepository orderRepository;
+    private final WebClient.Builder webClientBuilder;
 
     public void placeOrder(OrderRequest orderRequest) {
 
         // Check for inventory
-        
+        BaseResponse result = this.webClientBuilder.build()
+            .post()
+            .uri("http://localhost:8083/api/inventory/in-stock")
+            .bodyValue(orderRequest.getOrderItems())
+            .retrieve()
+            .bodyToMono(BaseResponse.class)
+            .block();
 
-        Order order = new Order();
-        order.setOrderNumber(UUID.randomUUID().toString());
-        order.setOrderItems(orderRequest.getOrderItems().stream().map(orderRequestItem -> mapOrderItemToOrderItem(orderRequestItem, order))
-                .toList());
-        this.orderRepository.save(order);
+        if (result != null && !result.hasErrors()){
+            Order order = new Order();
+            order.setOrderNumber(UUID.randomUUID().toString());
+            order.setOrderItems(orderRequest.getOrderItems().stream().map(orderRequestItem -> mapOrderItemToOrderItem(orderRequestItem, order))
+                    .toList());
+            this.orderRepository.save(order);
+        } else {
+            throw new IllegalArgumentException("Product is not in stock, please try again later");
+        }
     }
 
     private OrderItems mapOrderItemToOrderItem(OrderItemRequest orderItemRequest, Order order) {
